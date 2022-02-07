8e534d23a3d69bc6084b192b1d03e41706c4446b
ceramicowl <32081778+ceramicowl@users.noreply.github.com>
2022-02-06 19:27:06 -0500

    Create Parser.py

---
+++ Parser.py
@@ -0,0 +1,21 @@
+import urllib.request
+import json
+
+input_file = open('groups.txt', 'r')  # This file generated with data from the SLPC
+output_file = open('results.txt', 'w')
+
+output_file.write("Organization,URL\n")
+
+line = input_file.readline()
+while line != "":
+    url = 'https://autocomplete.clearbit.com/v1/companies/suggest?query=' + line
+    url = url.replace(" ", "_")
+    with urllib.request.urlopen(url) as response:
+        html = json.loads(response.read())
+        if html:
+            output_file.write(html[0]['domain'] + "\n")
+    line = input_file.readline()
+
+input_file.close()
+output_file.close()
+print("JOB DONE")
