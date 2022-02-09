2ba9864ef0ff10ac4b155908455589cc43dcb5a9
ceramicowl <32081778+ceramicowl@users.noreply.github.com>
2022-02-09 15:10:05 -0500

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -6,15 +6,15 @@ output_file = open('results.txt', 'w')
 
 line = input_file.readline()
 while line != "":
-    append = line.replace(" ", "_") # Lines 9 & 10 change the organization name to proper HTML format
+    append = line.replace(" ", "_")  # Lines 9 & 10 change the organization name to proper HTML format
     append = line.replace("/", "&sol;")
     url = 'https://autocomplete.clearbit.com/v1/companies/suggest?query=' + append
     with urllib.request.urlopen(url) as response:
-        html = json.loads(response.read()) # The URL request returns data in JSON format, which is read here
+        html = json.loads(response.read())  # The URL request returns data in JSON format, which is read here
         if html:
-            output_file.write(html[0]['domain'] + "\n") # The JSON data is converted to a list of dicts, and the first result is written to the output file
+            output_file.write(html[0]['domain'] + "\n")  # The JSON data is converted to a list of dicts, and the first result is written to the output file
     line = input_file.readline()
 
 input_file.close()
 output_file.close()
-print("JOB DONE") # So I know it's actully finished
+print("JOB DONE")  # So I know it's actully finished
