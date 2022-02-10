48ad6d5e85dfe40014229535eac38e14d05d7daf
KatDestroyer <elrodkathryn@gmail.com>
2022-02-10 15:48:49 +0100

    improved detection of year with/without data

--- Parser.py
+++ Parser.py
@@ -12,14 +12,17 @@ print('Downloading data for each year...')
 for year in range(this_year, 1999, -1):
     url = 'https://www.splcenter.org/hate-map/csv/'+str(year)
     response = requests.get(url)
-    if response.links: continue  # If response has links, it's not a valid year
-    csv_lines = response.text.splitlines()
-    encoded_csv = csv.reader(csv_lines)
-    print('Got '+str(len(csv_lines))+' names from '+str(year))
-    for row in encoded_csv:
-        name = row[0]
-        if not name in names:
-            names.append(name)
+    if response.headers['content-type'].split(';')[0] == 'text/csv':
+        # content-type will be something like 'text/csv;charset=utf-8'
+        # if we didn't get a csv, then there's no data for that year
+        csv_lines = response.text.splitlines()
+        encoded_csv = csv.reader(csv_lines)
+        print('Got '+str(len(csv_lines))+' groups from '+str(year))
+
+        for row in encoded_csv:
+            name = row[0]
+            if not name in names:
+                names.append(name)
 print('All data downloaded!')
 
 print('Finding associated websites for each group...')
