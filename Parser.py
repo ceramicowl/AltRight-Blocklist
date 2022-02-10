f565a07eeb0866da5e0e975dace878533175b0d6
KatDestroyer <elrodkathryn@gmail.com>
2022-02-10 15:37:47 +0100

    downloads all years (& some more verbosity)

--- Parser.py
+++ Parser.py
@@ -6,14 +6,23 @@ import requests
 import csv
 from datetime import date
 
+names = []
 this_year = date.today().year
-for year in range(this_year, 2000, -1):
+print('Downloading data for each year...')
+for year in range(this_year, 1999, -1):
     url = 'https://www.splcenter.org/hate-map/csv/'+str(year)
     response = requests.get(url)
-    if not response.links: break  # If response has links, it's not a valid year
-encoded_csv = csv.reader(response.text.splitlines())
-names = [row[0] for row in encoded_csv]
+    if response.links: continue  # If response has links, it's not a valid year
+    csv_lines = response.text.splitlines()
+    encoded_csv = csv.reader(csv_lines)
+    print('Got '+str(len(csv_lines))+' names from '+str(year))
+    for row in encoded_csv:
+        name = row[0]
+        if not name in names:
+            names.append(name)
+print('All data downloaded!')
 
+print('Finding associated websites for each group...')
 with open('results.txt', 'w') as output_file:
     for name in names:
         append = urllib.parse.quote(name, safe='')  # Encodes line for url
@@ -21,6 +30,10 @@ with open('results.txt', 'w') as output_file:
         with urllib.request.urlopen(url) as response:
             html = json.loads(response.read())  # The URL request returns data in JSON format, which is read here
             if html:
-                output_file.write(html[0]['domain'] + "\n")  # The JSON data is converted to a list of dicts, and the first result is written to the output file
+                website = html[0]['domain']
+                print(name+': '+website)
+                output_file.write(website + "\n")  # The JSON data is converted to a list of dicts, and the first result is written to the output file
+            else:
+                print('No website found for '+name)
 
 print("JOB DONE")  # So I know it's actully finished
