2bb4b5d0924f7f65b5da73cf00a6d47f73ec99f8
KatDestroyer <elrodkathryn@gmail.com>
2022-02-10 15:12:28 +0100

    moved downloader code to parser

--- Parser.py
+++ Parser.py
@@ -1,16 +1,26 @@
 import urllib.request
-import urllib.parse
 import json
 
-with open('groups.txt', 'r') as input_file, open('results.txt', 'w') as output_file:
-    line = input_file.readline()
-    while line != "":
-        append = urllib.parse.quote(line, safe='')
+import urllib.parse
+import requests
+import csv
+from datetime import date
+
+this_year = date.today().year
+for year in range(this_year, 2000, -1):
+    url = 'https://www.splcenter.org/hate-map/csv/'+str(year)
+    response = requests.get(url)
+    if not response.links: break  # If response has links, it's not a valid year
+encoded_csv = csv.reader(response.text.splitlines())
+names = [row[0] for row in encoded_csv]
+
+with open('results.txt', 'w') as output_file:
+    for name in names:
+        append = urllib.parse.quote(name, safe='')  # Encodes line for url
         url = 'https://autocomplete.clearbit.com/v1/companies/suggest?query=' + append
         with urllib.request.urlopen(url) as response:
             html = json.loads(response.read())  # The URL request returns data in JSON format, which is read here
             if html:
                 output_file.write(html[0]['domain'] + "\n")  # The JSON data is converted to a list of dicts, and the first result is written to the output file
-        line = input_file.readline()
 
 print("JOB DONE")  # So I know it's actully finished
